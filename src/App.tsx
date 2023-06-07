2c22b6884e16e18fc2e62ea9682138a6644ed36f
Raimones Barros <raimonesrg3@gmail.com>
2023-06-07 20:54:41 -0300

    feat: use defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/Global'
+import { defaultTheme } from './styles/theme/default'
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello, there!</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
