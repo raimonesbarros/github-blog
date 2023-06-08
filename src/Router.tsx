1444942733764f7aeb2595333d10a03cbedd612b
Raimones Barros <raimonesrg3@gmail.com>
2023-06-07 23:31:34 -0300

    feat: create defaut layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import { Blog } from './pages/Blog'
 import { Post } from './pages/Post'
+import { DefaultLayout } from './styles/layout/Default'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Blog />} />
-      <Route path="/post" element={<Post />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Blog />} />
+        <Route path="/post" element={<Post />} />
+      </Route>
     </Routes>
   )
 }
---
+++ src/styles/layout/Default.styles.ts
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components'
+
+export const DefaultLayoutContainer = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  height: 100vh;
+  margin: 0 auto;
+`
